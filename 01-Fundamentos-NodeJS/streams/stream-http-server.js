2071091591fc418a2fb6d0d150373a22b915db58
Nidsonr1 <nidsoncampos@hotmail.com>
2023-02-12 16:53:47 -0300

    Streams no NodeJS

---
+++ 01-Fundamentos-NodeJS/streams/stream-http-server.js
@@ -0,0 +1,30 @@
+import http from 'node:http'
+import { Transform } from 'node:stream'
+
+class InverseNumberString extends Transform {
+  _transform(chunk, encoding, callback) {
+    const transformed = Number(chunk.toString()) * -1
+
+    console.log(transformed);
+    callback(null, Buffer.from(String(transformed)))
+  }
+}
+
+const server = http.createServer(async (req, res) => {
+
+  const buffers = [];
+
+  for await (const chunk of req) {
+    buffers.push(chunk);
+  }
+
+  const fullStreamContent = Buffer.concat(buffers).toString()
+
+  console.log(fullStreamContent)
+  res.end(fullStreamContent)
+
+});
+
+server.listen(3334, () => {
+  console.log('Server Stream running')
+});
