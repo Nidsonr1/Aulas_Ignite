c5df227ff56af541022c8d7b3f2a4153afc9d476
Nidsonr1 <nidsoncampos@hotmail.com>
2023-08-29 11:20:54 -0300

    Configurando Eslint

--- 3-API-SOLID/src/env/index.ts
+++ 3-API-SOLID/src/env/index.ts
@@ -1,19 +1,17 @@
 import 'dotenv/config'
 import { z } from 'zod'
 
-//Validar as variáveis ambientes
-
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
-  PORT: z.coerce.number()
-});
+  PORT: z.coerce.number(),
+})
 
-const _env = envSchema.safeParse(process.env);
+const _env = envSchema.safeParse(process.env)
 
-if(_env.success === false) {
-  console.error('❌ - Invalid environment variables', _env.error.format());
+if (_env.success === false) {
+  console.error('❌ - Invalid environment variables', _env.error.format())
 
-  throw new Error('Invalid environment variables');
+  throw new Error('Invalid environment variables')
 }
 
 export const env = _env.data
